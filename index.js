35a7605518ae7cbeedc61ea1f04b9e53e165075c
Julio Moreno <julioc.morenob@gmail.com>
2021-05-22 13:51:49 -0400

    Displaying images

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-var objDrink1 = new Drink();
-
+// var objDrink1 = new Drink();
 async function displayAllDrinks(){
     var data1={};
+    var cocktailImg1= document.getElementById("cocktailImg1");
    try {
     const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail');
     data1 = response.data;
     // objDrink1.questions = data1.results;
-    document.getElementById("drinkId").innerHTML = data1.drinks[0].idDrink;
-    document.getElementById("drinkName").innerHTML = data1.drinks[0].strDrink;
-    document.getElementById("drinkPicture").src = data1.drinks[0].strDrinkThumb;
+    // document.getElementById("drinkId").innerHTML = data1.drinks[0].idDrink;
+    // document.getElementById("drinkName").innerHTML = data1.drinks[0].strDrink;
+    // document.getElementById("drinkPicture").src = data1.drinks[0].strDrinkThumb;
+    for(var i=0;i<=6;i++)
+    {
+        $("#cocktailImg"+i).attr("src", data1.drinks[i].strDrinkThumb);
+    }
+    
+    // span1.style.src = data1.drinks[0].strDrinkThumb;
+
+//     <span class="cocktailTile" id="cocktailTile1">
+//     <img
+//       src="cup-lemon-juice-wooden-board.jpg"
+//       alt="cocktail photo"
+//       width="250px"
+//       height="250px"
+//     />
+//   </span>
 
    } catch (error) {
        console.log(error);
--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-"use strict";
+// "use strict";
 
-let id = 11007;
+// let id = 11007;
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-  .then((results) => results.json())
-  .then((data) => data.drinks[0])
-  .then((next) => {
-    for (let i = 1; i < 10; i++) {
-      let currentIng = `strIngredient${i}`;
-      if (next[currentIng]) {
-        console.log(next[currentIng]);
-      }
-    }
-  });
+// fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+//   .then((results) => results.json())
+//   .then((data) => data.drinks[0])
+//   .then((next) => {
+//     for (let i = 1; i < 10; i++) {
+//       let currentIng = `strIngredient${i}`;
+//       if (next[currentIng]) {
+//         console.log(next[currentIng]);
+//       }
+//     }
+//   });
 
-class Order {
-  // purpose: build the values of the order
-  //   constatns: idOrder, drinksArr, subtotal, tax, total
-  // methods: balance after order is completed
+// class Order {
+//   // purpose: build the values of the order
+//   //   constatns: idOrder, drinksArr, subtotal, tax, total
+//   // methods: balance after order is completed
 
-  constructor(idOrder, drinksArr, subtotal, tax, total) {
-    //   assingment of this variables
-  }
+//   constructor(idOrder, drinksArr, subtotal, tax, total) {
+//     //   assingment of this variables
+//   }
 
-  getDrink() {
-    //   use a new Drink to push to the object
-    // add new drink to order;
-    //;
-  }
+//   getDrink() {
+//     //   use a new Drink to push to the object
+//     // add new drink to order;
+//     //;
+//   }
 
-  //   getScore() {
-  //     return this.score;
-  //   }
+//   //   getScore() {
+//   //     return this.score;
+//   //   }
 
-  //   getWins() {
-  //     return this.wins;
-  //   }
+//   //   getWins() {
+//   //     return this.wins;
+//   //   }
 
-  //   getLosses() {
-  //     return this.losses;
-  //   }
+//   //   getLosses() {
+//   //     return this.losses;
+//   //   }
 
-  //   getQuestions() {
-  //     return this.questions;
-  //   }
-}
+//   //   getQuestions() {
+//   //     return this.questions;
+//   //   }
+// }
 
-class Drinks {
-  // when we click on a drink, this will fill the drinksArr in the Order
-  // All of the
-  // first api gives the drink name, and ID
-  // second gives everything
-}
+// class Drinks {
+//   // when we click on a drink, this will fill the drinksArr in the Order
+//   // All of the
+//   // first api gives the drink name, and ID
+//   // second gives everything
+// }
