5fb6b7dee139c70e6a3170ef5588a55c1eda6ae3
Julio Moreno <julioc.morenob@gmail.com>
2021-05-22 20:27:51 -0400

    Removing script commented code

--- index.js
+++ index.js
@@ -5,26 +5,12 @@ async function displayAllDrinks(){
    try {
     const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail');
     data1 = response.data;
-    // objDrink1.questions = data1.results;
-    // document.getElementById("drinkId").innerHTML = data1.drinks[0].idDrink;
-    // document.getElementById("drinkName").innerHTML = data1.drinks[0].strDrink;
-    // document.getElementById("drinkPicture").src = data1.drinks[0].strDrinkThumb;
+   
     for(var i=0;i<=6;i++)
     {
         $("#cocktailImg"+i).attr("src", data1.drinks[i].strDrinkThumb);
     }
-    
-    // span1.style.src = data1.drinks[0].strDrinkThumb;
-
-//     <span class="cocktailTile" id="cocktailTile1">
-//     <img
-//       src="cup-lemon-juice-wooden-board.jpg"
-//       alt="cocktail photo"
-//       width="250px"
-//       height="250px"
-//     />
-//   </span>
-
+   
    } catch (error) {
        console.log(error);
    }
