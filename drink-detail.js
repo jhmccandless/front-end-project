99869cdd6041367dfac767dd7cbbdd1586a7c82a
Cassie Anderson <cbp.smith@gmail.com>
2021-05-26 22:28:54 -0400

    Brought data to drink-detail.html to populate drink image and instructions

--- drink-detail.js
+++ drink-detail.js
@@ -5,11 +5,17 @@ async function displayDrinkDetail() {
         console.log(drinkSelected);
         const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+drinkSelected);
         data1 = response.data;
-        console.log(data1);}
-        catch(error)
+        console.log(data1);
+
+        $(".ins-text").html(data1.drinks[0].strInstructions);
+        $("#drink-image").attr("src", data1.drinks[0].strDrinkThumb);
+        
+    }
+    catch(error){
         {
             console.log(error);
         }
     }
+}
 
-    displayDrinkDetail();
+displayDrinkDetail();
