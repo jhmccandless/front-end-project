7f15ecb85bfabadcc9021a0bd0c8650bf93a92c4
Jason McCandless <jhmccandless1105@gmail.com>
2021-05-27 19:54:26 -0500

    working as a group to update drink-detail page

--- drink-detail.js
+++ drink-detail.js
@@ -1,21 +1,35 @@
-var data1={};
+var data1 = {};
 async function displayDrinkDetail() {
-    try {
-        var drinkSelected = localStorage.getItem("drinkName");
-        console.log(drinkSelected);
-        const response = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+drinkSelected);
-        data1 = response.data;
-        console.log(data1);
+  try {
+    var drinkSelected = localStorage.getItem("drinkName");
+    console.log(drinkSelected);
+    const response = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" +
+        drinkSelected
+    );
+    data1 = response.data;
+    console.log(data1);
 
-        $(".ins-text").html(data1.drinks[0].strInstructions);
-        $("#drink-image").attr("src", data1.drinks[0].strDrinkThumb);
-        
+    $(".sel-drink-title").html(data1.drinks[0].strDrink);
+    $(".ins-text").html(data1.drinks[0].strInstructions);
+    $("#drink-image").attr("src", data1.drinks[0].strDrinkThumb);
+
+    for (let i = 1; i < 15; i++) {
+      let currentIng = `strIngredient${i}`;
+      if (data1.drinks[0][currentIng]) {
+        document
+          .querySelector(".ing-list")
+          .insertAdjacentHTML(
+            "beforeend",
+            `<li>${data1.drinks[0][currentIng]}</li>`
+          );
+      }
     }
-    catch(error){
-        {
-            console.log(error);
-        }
+  } catch (error) {
+    {
+      console.log(error);
     }
+  }
 }
 
 displayDrinkDetail();
