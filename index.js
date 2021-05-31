aef5cd5da049a6451c976e7e9880bf780c7b9312
Julio Moreno <julioc.morenob@gmail.com>
2021-05-30 23:00:05 -0400

    Adding drinks to Order

--- index.js
+++ index.js
@@ -4,61 +4,89 @@ var data1 = {};
 var data2 = {};
 var desiredDrinks = [];
 
+
 async function displayAllDrinks() {
-  try {
-    const response = await axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
-    );
-    data1 = response.data;
-    // console.log(data1);
+    try {
+        const response = await axios.get(
+            "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+        );
+        data1 = response.data;
+        // console.log(data1);
 
-    for (let i = 0; i < data1.drinks.length; i++) {
-      if (data1.drinks[i].strDrink == "Mojito") {
-        desiredDrinks.push(i);
-      }
+        for (let i = 0; i < data1.drinks.length; i++) {
+            if (data1.drinks[i].strDrink == "Mojito") {
+                desiredDrinks.push(i);
+            }
 
-      if (data1.drinks[i].strDrink == "Munich Mule") {
-        desiredDrinks.push(i);
-      }
-      if (data1.drinks[i].strDrink == "Michelada") {
-        desiredDrinks.push(i);
-      }
-      if (data1.drinks[i].strDrink == "Gin and Soda") {
-        desiredDrinks.push(i);
-      }
-      if (data1.drinks[i].strDrink == "Death in the Afternoon") {
-        desiredDrinks.push(i);
-      }
-      if (data1.drinks[i].strDrink == "Cosmopolitan") {
-        desiredDrinks.push(i);
-      }
+            if (data1.drinks[i].strDrink == "Munich Mule") {
+                desiredDrinks.push(i);
+            }
+            if (data1.drinks[i].strDrink == "Michelada") {
+                desiredDrinks.push(i);
+            }
+            if (data1.drinks[i].strDrink == "Gin and Soda") {
+                desiredDrinks.push(i);
+            }
+            if (data1.drinks[i].strDrink == "Death in the Afternoon") {
+                desiredDrinks.push(i);
+            }
+            if (data1.drinks[i].strDrink == "Cosmopolitan") {
+                desiredDrinks.push(i);
+            }
+        }
+
+        for (var i = 0; i < desiredDrinks.length; i++) {
+            $("#cocktailImg" + [i + 1]).attr(
+                "src",
+                data1.drinks[desiredDrinks[i]].strDrinkThumb
+            );
+            $("#cocktailImg" + [i + 1]).attr(
+                "alt",
+                data1.drinks[desiredDrinks[i]].strDrink
+            );
+            $("#cocktailTile" + [i + 1] + "Label").html(
+                data1.drinks[desiredDrinks[i]].strDrink
+            );
+        }
+    } catch (error) {
+        console.log(error);
     }
-
-    for (var i = 0; i < desiredDrinks.length; i++) {
-      $("#cocktailImg" + [i + 1]).attr(
-        "src",
-        data1.drinks[desiredDrinks[i]].strDrinkThumb
-      );
-      $("#cocktailImg" + [i + 1]).attr(
-        "alt",
-        data1.drinks[desiredDrinks[i]].strDrink
-      );
-      $("#cocktailTile" + [i + 1] + "Label").html(
-        data1.drinks[desiredDrinks[i]].strDrink
-      );
-    }
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 function displayDrinkDetails(num) {
-  try {
-    var currNum = num;
-    localStorage.setItem("drinkName", currNum.alt);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        var currNum = num;
+        localStorage.setItem("drinkName", currNum.alt);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-window.onload = displayAllDrinks();
+function addToOrder() {
+    var orderNumber = 0;
+    try {
+        if (localStorage.getItem("drinkName") && localStorage.getItem("orderInProgress") === null) {
+            let startOrder = new Order();
+            startOrder.idOrder = 1;
+            let drinkToBeAdded = JSON.parse(localStorage.getItem("drinkAdded"));
+            startOrder.drinksArr.push(drinkToBeAdded);
+            let OrderToBeAdded_Serialized = JSON.stringify(startOrder);
+            localStorage.setItem('orderInProgress', OrderToBeAdded_Serialized);
+            console.log("starting a new order");
+        } else if (localStorage.getItem("drinkName") && localStorage.getItem("orderInProgress")) {
+          let continueOrder = JSON.parse(localStorage.getItem("orderInProgress"));
+            let drinkToBeAdded = JSON.parse(localStorage.getItem("drinkAdded"));
+            continueOrder.drinksArr.push(drinkToBeAdded);
+            let OrderToBeAdded_Serialized = JSON.stringify(continueOrder);
+            localStorage.setItem('orderInProgress', OrderToBeAdded_Serialized);
+            console.log("added a new drink");
+        } else {
+            console.log("No drinks added");
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+addToOrder();
+displayAllDrinks();
--- order.js
+++ order.js
@@ -1,37 +1,34 @@
 class Order {
-  // purpose: build the values of the order
-  //   constatns: idOrder, drinksArr, subtotal, tax, total
-  // methods: balance after order is completed
+    // purpose: build the values of the order
+    //   constatns: idOrder, drinksArr, subtotal, tax, total
+    // methods: balance after order is completed
 
-  constructor(idOrder, drinksArr, subtotal, tax, total) {
-    //   assingment of this variables
-  }
+    constructor(_idOrder = 0, _drinksArr = [], _subTotal = 0.00, _tax = 0.00, _total = 0.00) {
+        this.idOrder = _idOrder;
+        this.drinksArr = _drinksArr;
+        this.subTotal = _subTotal;
+        this.tax = _tax;
+        this.total = _total;
+        
+    }
+    getIdOrder() {
+        return this.idOrder;
+    }
+
+    getDrinksArr() {
+        return drinksArr;
+    }
+
+    getSubTotal() {
+        return subTotal;
+    }
+
+    getTax() {
+        return price;
+    }
+
+    getTotal() {
+        return idDrink;
+    }
+    
 }
-
-console.log();
-
-//     async createOrder() {
-//         var data1 = [];
-//         var data2 = [];
-//         var player1 = "";
-//         var player2 = "";
-
-//         player1 = document.getElementById("player1").value;
-//         player2 = document.getElementById("player2").value;
-//         if (player1 === "" || player2 === "") {
-//             alert("Player fields cannot be empty");
-//             return;
-//         }
-//         var objPlayer1 = new Player(player1);
-//         var objPlayer2 = new Player(player2);
-
-//         const response = await axios.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple');
-//         data1 = response.data;
-//         objPlayer1.questions = data1.results;
-
-//         const response2 = await axios.get('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple')
-//         data2 = response2.data;
-//         objPlayer2.questions = data2.results;
-
-//         this.players = [objPlayer1, objPlayer2];
-//     }
