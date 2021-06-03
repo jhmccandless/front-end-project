2238c45a3846457ebf7846cbec435497fcc7b8db
Jason McCandless <jhmccandless1105@gmail.com>
2021-06-02 19:11:59 -0500

    corrected errors

--- drink-detail.js
+++ drink-detail.js
@@ -14,45 +14,39 @@ async function displayDrinkDetail() {
     $(".ins-text").html(data1.drinks[0].strInstructions);
     $("#drink-image").attr("src", data1.drinks[0].strDrinkThumb);
 
-        for (let i = 1; i < 15; i++) {
-            let currentIng = `strIngredient${i}`;
-            let currentMeasure = `strMeasure${i}`;
-            if (data1.drinks[0][currentIng]) {
-                document
-                    .querySelector(".ing-list")
-                    .insertAdjacentHTML(
-                        "beforeend",
-                        `<li>${data1.drinks[0][currentMeasure]} ${data1.drinks[0][currentIng]}</li>`
-                    );
-            }
-        }
-
-        var drinkPrice = 0;
-        if(drinkSelected === "Cosmopolitan"){
-        drinkPrice = 6.00;
-        }
-        if(drinkSelected === "Death in the Afternoon"){
-        drinkPrice = 4.00;
-        }
-        if(drinkSelected === "Gin and Soda"){
-        drinkPrice = 4.00;
-        }
-        if(drinkSelected === "Michelada"){
-        drinkPrice = 5.50;
-        }
-        if(drinkSelected === "Mojito"){
-        drinkPrice = 5.00;
-        }
-        if(drinkSelected === "Munich Mule"){
-        drinkPrice = 5.50;
-        }
-        $('#drinkPrice').html(drinkPrice);
-    
-    } catch (error) {
-        {
-            console.log(error);
-        }
+    for (let i = 1; i < 15; i++) {
+      let currentIng = `strIngredient${i}`;
+      let currentMeasure = `strMeasure${i}`;
+      if (data1.drinks[0][currentIng]) {
+        document
+          .querySelector(".ing-list")
+          .insertAdjacentHTML(
+            "beforeend",
+            `<li>${data1.drinks[0][currentMeasure]} ${data1.drinks[0][currentIng]}</li>`
+          );
+      }
     }
+
+    var drinkPrice = 0;
+    if (drinkSelected === "Cosmopolitan") {
+      drinkPrice = 6.0;
+    }
+    if (drinkSelected === "Death in the Afternoon") {
+      drinkPrice = 4.0;
+    }
+    if (drinkSelected === "Gin and Soda") {
+      drinkPrice = 4.0;
+    }
+    if (drinkSelected === "Michelada") {
+      drinkPrice = 5.5;
+    }
+    if (drinkSelected === "Mojito") {
+      drinkPrice = 5.0;
+    }
+    if (drinkSelected === "Munich Mule") {
+      drinkPrice = 5.5;
+    }
+    $("#drinkPrice").html(drinkPrice);
   } catch (error) {
     {
       console.log(error);
@@ -61,43 +55,38 @@ async function displayDrinkDetail() {
 }
 
 function addToCart() {
-    try {
-        let drinkAdded = new Drink();
-        let selectedIngridients = [];
-        drinkAdded.idDrink = data1.drinks[0].idDrink;
-        drinkAdded.strDrink = data1.drinks[0].strDrink;
-        drinkAdded.strInstructions = data1.drinks[0].strInstructions;
-        drinkAdded.price = document.getElementById('drinkPrice').innerHTML;
-        
+  try {
+    let drinkAdded = new Drink();
+    let selectedIngridients = [];
+    drinkAdded.idDrink = data1.drinks[0].idDrink;
+    drinkAdded.strDrink = data1.drinks[0].strDrink;
+    drinkAdded.strInstructions = data1.drinks[0].strInstructions;
+    drinkAdded.price = document.getElementById("drinkPrice").innerHTML;
 
-        for (let i = 1; i < 15; i++) {
-            let currentIng = `strIngredient${i}`;
-            let currentMeasure = `strMeasure${i}`;
-            if (data1.drinks[0][currentIng]) {
-                drinkAdded.strIngredients[i - 1] = data1.drinks[0][currentMeasure] + data1.drinks[0][currentIng];
-            } else {
-                break;
-            }
-        }
-        drinkAdded.strDrinkThumb = data1.drinks[0].strDrinkThumb;
-        let drinkAdded_serialized = JSON.stringify(drinkAdded);
-        localStorage.setItem("drinkAdded", drinkAdded_serialized);
-        localStorage.removeItem("drinkName");
-
-        //need waht's on the cart object to figure out how many drinks and what cost for total 
-        // var tax = 0.10;
-        // var subtotal = '';//get prices off order object
-        // var taxAmt = subtotal * tax;
-        // var total = taxAmt + subtotal;
-
-        // $('#subtotal').html(subtotal);
-        // $('#taxAmt').html(taxAmt);
-        // $('#totalAmt').html(total);
-
+    for (let i = 1; i < 15; i++) {
+      let currentIng = `strIngredient${i}`;
+      let currentMeasure = `strMeasure${i}`;
+      if (data1.drinks[0][currentIng]) {
+        drinkAdded.strIngredients[i - 1] =
+          data1.drinks[0][currentMeasure] + data1.drinks[0][currentIng];
+      } else {
+        break;
+      }
     }
-    catch (error) {
-        console.log(error);
-    }
+    drinkAdded.strDrinkThumb = data1.drinks[0].strDrinkThumb;
+    let drinkAdded_serialized = JSON.stringify(drinkAdded);
+    localStorage.setItem("drinkAdded", drinkAdded_serialized);
+    localStorage.removeItem("drinkName");
+
+    //need waht's on the cart object to figure out how many drinks and what cost for total
+    // var tax = 0.10;
+    // var subtotal = '';//get prices off order object
+    // var taxAmt = subtotal * tax;
+    // var total = taxAmt + subtotal;
+
+    // $('#subtotal').html(subtotal);
+    // $('#taxAmt').html(taxAmt);
+    // $('#totalAmt').html(total);
     drinkAdded.strDrinkThumb = data1.drinks[0].strDrinkThumb;
     let drinkAdded_serialized = JSON.stringify(drinkAdded);
     localStorage.setItem("drinkAdded", drinkAdded_serialized);
