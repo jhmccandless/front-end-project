72829b90aa166ab91de86cae6c5c0e3daef759b4
Gabrielle Pugh <gabriellepugh30@gmail.com>
2021-06-02 23:33:31 -0400

    no new changes yet

--- cart.js
+++ cart.js
@@ -2,7 +2,7 @@ function fillOrderItems(){
     try{
     if(localStorage.getItem(orderInProgress))
     {
-        let cartItems_des = JSON.parse(localStorage.getItem("orderInProgress")) ;
+        let cartItems_des = JSON.parse(localStorage.getItem("orderInProgress"));
         for (let i = 0; cartItems_des.drinksArr.lenght; i++) {
            
 
@@ -13,10 +13,15 @@ catch(error)
 {
     console.log(error);
 }
+
+var removeCartItems = document.getElementsByClassName('cart-items')
+console.log(removeCartItems)
+for (var i = 0; i < removeCartItems.length; i++) {
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
 }
-
-
-
-
 
 
